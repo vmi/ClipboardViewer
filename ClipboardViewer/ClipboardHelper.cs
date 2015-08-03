86fa53ef66b9b2f51188782710473f9586eac2c2
Motonori IWAMURO <vmi@nifty.com>
2015-08-04 07:49:37 +0900

    1st commit.

---
+++ ClipboardViewer/ClipboardHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace ClipboardViewer
+{
+    public class ClipboardHelper
+    {
+        private const int WM_DRAWCLIPBOARD = 0x0308;
+        private const int WM_CHANGECBCHAIN = 0x030D;
+
+        private Action callback;
+        private HwndSource hWndSource = null;
+        private IntPtr nextHandle;
+
+        [DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+
+        [DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
+        private extern static int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        public void RegisterHandler(Window window, Action callback)
+        {
+            if (hWndSource != null)
+                throw new InvalidOperationException("Handler is already registered.");
+            this.callback = callback;
+            WindowInteropHelper wih = new WindowInteropHelper(window);
+            this.hWndSource = HwndSource.FromHwnd(wih.Handle);
+            this.hWndSource.AddHook(WndProc);
+            this.nextHandle = SetClipboardViewer(hWndSource.Handle);
+        }
+
+        public void DeregisterHandler()
+        {
+            if (hWndSource == null)
+                return;
+            ChangeClipboardChain(hWndSource.Handle, nextHandle);
+            hWndSource.RemoveHook(WndProc);
+            hWndSource = null;
+            nextHandle = IntPtr.Zero;
+            callback = null;
+        }
+
+        public bool IsRegistered()
+        {
+            return hWndSource != null;
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    try
+                    {
+                        callback.Invoke();
+                    }
+                    catch (Exception e)
+                    {
+                        DeregisterHandler();
+                        throw e;
+                    }
+                    SendMessage(nextHandle, msg, wParam, lParam);
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (wParam == nextHandle)
+                        nextHandle = lParam;
+                    else if (nextHandle != IntPtr.Zero)
+                        SendMessage(nextHandle, msg, wParam, lParam);
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+    }
+}
---
+++ ClipboardViewer/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace ClipboardViewer
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static Regex TOKEN_RE = new Regex(@"\G(?:(?<sp>\r\n|[\p{Cc}\p{Zs}])|(?<ch>[^\p{Cc}\p{Zs}]+))", RegexOptions.Singleline);
+
+        private static Dictionary<string, string> SP_MAP = new Dictionary<string, string>()
+        {
+            { "\r\n", "⏎" },
+            { "\n", "↲" },
+            { "\r", "↤" },
+            { "\t", "↦" },
+            { " ", "␣" },
+            { "\u3000", "□" },
+        };
+        private ClipboardHelper clipboardHelper = new ClipboardHelper();
+        private FontFamily fontFamily = new FontFamily("Meiryo");
+        private double fontSize = 12;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Paragraph NextParagraph(Paragraph prevParagraph)
+        {
+            if (prevParagraph?.Inlines.Count > 0)
+                richTextBox.Document.Blocks.Add(prevParagraph);
+            var paragraph = new Paragraph();
+            paragraph.FontFamily = fontFamily;
+            paragraph.FontSize = fontSize;
+            return paragraph;
+        }
+
+        private void DrawClipboard()
+        {
+            richTextBox.Document.Blocks.Clear();
+            var paragraph = NextParagraph(null);
+            if (Clipboard.ContainsText())
+            {
+                var text = Clipboard.GetText();
+                foreach (Match match in TOKEN_RE.Matches(text))
+                {
+                    var sp = match.Groups["sp"];
+                    if (sp.Length > 0)
+                    {
+                        switch (sp.Value)
+                        {
+                            case "\r\n":
+                            case "\r":
+                            case "\n":
+                                var nl = new Run("⏎");
+                                nl.Foreground = Brushes.Red;
+                                paragraph.Inlines.Add(nl);
+                                paragraph = NextParagraph(paragraph);
+                                break;
+
+                            case "\t":
+                                var tab = new TextBlock(new Run("↦"));
+                                tab.Foreground = Brushes.Red;
+                                tab.LayoutTransform = new ScaleTransform(2, 1);
+                                paragraph.Inlines.Add(tab);
+                                break;
+
+                            case " ":
+                                var spc = new TextBlock(new Run("␣"));
+                                spc.Foreground = Brushes.Red;
+                                spc.Padding = new Thickness(1, 0, 1, 0);
+                                spc.LayoutTransform = new ScaleTransform(0.75, 1);
+                                paragraph.Inlines.Add(spc);
+                                break;
+
+                            case "\u3000":
+                                var wspc = new Run("□");
+                                wspc.Foreground = Brushes.Red;
+                                paragraph.Inlines.Add(wspc);
+                                break;
+
+                            default:
+                                var esc = new Run(string.Format(@"\u{0:X4}", sp.Value[0]));
+                                esc.Foreground = Brushes.Red;
+                                paragraph.Inlines.Add(esc);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        paragraph.Inlines.Add(new Run(match.Groups["ch"].Value));
+                    }
+                }
+            }
+            else
+            {
+                var notText = new Run("(not text)");
+                notText.Foreground = Brushes.Firebrick;
+                notText.FontStyle = FontStyles.Italic;
+                paragraph.Inlines.Add(notText);
+            }
+            if (paragraph.Inlines.Count > 0)
+                richTextBox.Document.Blocks.Add(paragraph);
+        }
+
+        private void ClipboardViewer_ContentRendered(object sender, EventArgs e)
+        {
+            if (!clipboardHelper.IsRegistered())
+                clipboardHelper.RegisterHandler(this, DrawClipboard);
+
+        }
+
+        private void ClipboardViewer_Closed(object sender, EventArgs e)
+        {
+            if (clipboardHelper.IsRegistered())
+                clipboardHelper.DeregisterHandler();
+        }
+    }
+}
